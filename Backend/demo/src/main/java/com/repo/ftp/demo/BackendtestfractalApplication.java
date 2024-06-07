4e6858f15bacf55747c9f44039fed7a53101f2a6
DiegoD <a20171086@pucp.edu.pe>
2024-06-07 11:56:47 -0500

    UpdateBackend

---
+++ Backend/demo/src/main/java/com/repo/ftp/demo/BackendtestfractalApplication.java
@@ -0,0 +1,13 @@
+package com.repo.ftp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtestfractalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtestfractalApplication.class, args);
+	}
+
+}
